3b41fb3e094527ddaed01e5e460811edb88286dc
vlad <vlad.dracula17@mail.ru>
2022-05-04 20:57:37 +0300

    Регистрационная регистрация жёстко работает(но я не проверял, так как челы не хотят сервер запустить)

--- app/src/main/java/com/example/test/register.java
+++ app/src/main/java/com/example/test/register.java
@@ -1,13 +1,92 @@
 package com.example.test;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class register  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+    }
+
+    public void Try_reg(View view){
+        String login = ((EditText) findViewById(R.id.login2)).getText().toString();
+        String mail = ((EditText) findViewById(R.id.email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordt1)).getText().toString();
+        String password_r = ((EditText) findViewById(R.id.passwordt2)).getText().toString();
+        if (password.equals(password_r) && password.length() > 3){
+            AsyncRequest a = new AsyncRequest();
+            String ans = a.doInBackground(login, password, mail);
+            if (ans.equals("busy")) {
+                ((EditText) findViewById(R.id.login)).setText("");
+                Toast.makeText(this, "К сожалению, такой логин занят", Toast.LENGTH_LONG).show();
+            } else {
+                Intent intent = new Intent(this, Enter.class);
+                startActivity(intent);
+                this.finish();
+            }
+        }
+    }
+
+    static class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(Context context) {
+            // конструктор суперкласса
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("create table sq ("
+                    + "id integer primary key autoincrement,"
+                    + "yes text," + "my_id integer" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+
+    static class AsyncRequest extends AsyncTask<String, Integer, String> {
+
+        String domen = "a339-178-72-68-143.ngrok.io";
+
+        @Override
+        protected String doInBackground(String... arg) {
+            String url = "https://" + domen + "/check_user?l=" + arg[0] + "&p=" + arg[1];
+            StringBuffer response;
+            try {
+                URL obj = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+                connection.setRequestMethod("GET");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                return response.toString();
+            } catch (Exception e) {
+                System.out.println("ERROR: " + e);
+                return "error";
+            }
+        }
     }
 }
