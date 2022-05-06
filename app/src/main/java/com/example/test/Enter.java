f4135e225e9dc99b111f59f4169abff5b484f467
Vlad <anikanovvlad@yandex.ru>
2022-05-06 10:46:29 +0500

    da

--- app/src/main/java/com/example/test/Enter.java
+++ app/src/main/java/com/example/test/Enter.java
@@ -130,7 +130,7 @@ public class Enter extends AppCompatActivity {
 
     static class AsyncRequest extends AsyncTask<String, Integer, String> {
 
-        String domen = "1ac8-178-178-90-188.eu.ngrok.io";
+        String domen = "7f5b-178-178-90-188.eu.ngrok.io";
 
         @Override
         protected String doInBackground(String... arg) {
--- app/src/main/java/com/example/test/register.java
+++ app/src/main/java/com/example/test/register.java
@@ -154,7 +154,7 @@ public class register extends AppCompatActivity {
 
     static class AsyncRequest extends AsyncTask<String, Integer, String> {
 
-        String domen = "1ac8-178-178-90-188.eu.ngrok.io";
+        String domen = "7f5b-178-178-90-188.eu.ngrok.io";
 
         @Override
         protected String doInBackground(String... arg) {
